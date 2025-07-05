29f4e41a1c033ca3795ab5b84b018eda5c6aeaaf
mirado <hajamirado10@gmail.com>
2025-07-05 23:18:05 +0300

    fix state

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -30,14 +30,14 @@ const RESOURCES = {"assets/asset/font/NotoSansSymbols-Regular.ttf": "9cf26c11c49
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "8a78a0de2ee2bdaa711570d7098950ec",
+"flutter_bootstrap.js": "75a23c25492254397b98e5eb1f96a46d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "e9130bd591673e6fec8e4271869cd244",
 "/": "e9130bd591673e6fec8e4271869cd244",
-"main.dart.js": "7891ce5e933a41ce227161c3bf04292e",
+"main.dart.js": "9e247b668a6cdfd90bf3b0958d9fd6af",
 "manifest.json": "dea0aa341c6d970617d20f828a188360",
 "version.json": "1f7b553cc2130f047abd4c61141ccda7"};
 // The application shell files that are downloaded before a service worker can
